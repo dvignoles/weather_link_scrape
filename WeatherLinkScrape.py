2dc1ac3a3c237e562f172c860b5443182931c944
Daniel Vignoles <dvignoles@gmail.com>
2019-02-07 17:57:08 -0500

    sqlite database setup

--- WeatherLinkScrape.py
+++ WeatherLinkScrape.py
@@ -12,6 +12,7 @@ from re import search
 from datetime import datetime
 from os import getcwd,chdir,makedirs
 from bs4 import BeautifulSoup
+from sqlalchemy import create_engine, MetaData,Table, Column, String, Integer, Float
 
 ###---CONFIG---###
 #ElementTree.Element name of Observation Time attribute
@@ -29,15 +30,67 @@ OBSERVATION_STR = '%Y-%m-%d_%H-%M'
 CURRENT = [
     'observation_time_rfc822','station_name','dewpoint_c','dewpoint_f','heat_index_c','heat_index_f',
     'location','latitude','longitude','pressure_in','pressure_mb','relative_humidity','solar_radiation',
-    'sunrise','sunset','temp_c','temp_f','uv_index','wind_degrees','wind_dir','wind_kt','wind_mph>',
+    'sunrise','sunset','temp_c','temp_f','uv_index','wind_degrees','wind_dir','wind_kt','wind_mph',
     'windchill_c','windchill_f'
 ]
 
+def db_init(url):
+    engine = create_engine('sqlite:///test.sqlite')
+    metadata = MetaData()
+
+    data = Table('data',metadata,
+        Column('observation_time',String(255),unique=True),
+        Column('station_name', String(255),unique=False),
+        Column('dewpoint_c', Float(), unique=False),
+        Column('dewpoint_f', Float(), unique=False),
+        Column('heat_index_c', Float(), unique=False),
+        Column('heat_index_f', Float(), unique=False),
+        Column('location', String(255), unique=False),
+        Column('latitude', Float(), unique=False),
+        Column('longitude', Float(), unique=False),
+        Column('pressure_in', Float(), unique=False),
+        Column('pressure_mb', Float(), unique=False),
+        Column('relative_humidity', Integer(),unique=False),
+        Column('solar_radiation', Integer(), unique=False),
+        Column('sunrise', String(255), unique=False),
+        Column('sunset', String(255), unique=False),
+        Column('temp_c', Float(), unique=False),
+        Column('temp_f', Float(), unique=False),
+        Column('uv_index', Float(), unique=False),
+        Column('wind_degress', Integer(), unique=False),
+        Column('wind_dir', String(255), unique=False),
+        Column('wind_kt', Float(), unique=False),
+        Column('wind_mph', Float(), unique=False),
+        Column('windchill_c', Float(), unique=False),
+        Column('windchill_f', Float(), unique=False)
+    )
+
+    metadata.create_all(engine)
+
+    #TODO: Insert Values of get_soup into table, deal with duplicates, and check this works in script
+
 def get_soup(url):
+    """
+        Return a dictionary of the scraped CURRENT variables from url
+    """
+
     soup = BeautifulSoup(simple_get(url),'xml')
     observations = list(map(lambda tag:soup.find(tag),CURRENT))
-    return observations
-    #note to self, access element.Tag.name,element.Tag
+
+    results = {}
+    for obs in observations:
+        tag = obs.name
+        content = obs.contents[0]
+
+        try:
+            results[tag] = int(content)
+        except ValueError:
+            try: 
+                results[tag] = float(content)
+            except:
+                results[tag] = content
+
+    return results
 
 def write_xml(url):
     """
@@ -45,7 +98,6 @@ def write_xml(url):
         write_xml writes a .xml to data folder in the appropriate year/month/day/ path
     """
     root_dir = getcwd()
-    #chdir(data_dir)
 
     tree = get_tree(url)
     date_time = get_obs_time(tree)
